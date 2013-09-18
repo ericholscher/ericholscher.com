397f8770ba8296bd47027a7887992bd52ca4bca4
Eric Holscher <eric@ericholscher.com>
2013-09-18 14:46:54 -0700

    Small changes

--- site/conf.py
+++ site/conf.py
@@ -106,7 +106,7 @@ html_theme = 'kr'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Surfing in Kansas"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
--- site/index.rst
+++ site/index.rst
@@ -1,3 +1,6 @@
+Home
+====
+
 Welcome to the home of Eric Holscher on the web. 
 
 I talk about software development, mostly in the realm of Django. I am interested in the web, testing, mobile apps, documentation, and much more.
