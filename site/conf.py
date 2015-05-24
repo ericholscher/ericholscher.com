db44f1d6577253fd81096b2a6020377c8dba9daf
Eric Holscher <eric@ericholscher.com>
2015-05-24 15:11:19 -0700

    Bump copyright

--- site/conf.py
+++ site/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Surfing in Kansas'
-copyright = u'2014, Eric Holscher'
+copyright = u'2015, Eric Holscher'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
