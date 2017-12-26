dd91437cc2ba40d967360d42eec139766571499a
Eric Holscher <eric@ericholscher.com>
2017-12-26 08:40:09 -0800

    Futz with publishing

--- drafts/publish.py
+++ drafts/publish.py
@@ -5,7 +5,7 @@ import datetime
 
 now = datetime.datetime.now()
 month = now.strftime('%b').lower()
-dir = "../%s/%s/%s/" % (now.year, month, now.day)
+dir = "../site/blog/%s/%s/%s/" % (now.year, month, now.day)
 
 os.system('mkdir -p %s' % dir)
 shutil.copy(sys.argv[1], dir)
